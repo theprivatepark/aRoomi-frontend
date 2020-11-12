bdbd23380a84e2648456e2d2172ac453f586bdbd
theprivatepark <anneypark@gmail.com>
2020-11-12 01:37:41 -0500

    added a profile page card and moved form to that

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const ProfileCard = () => {
+  return(
+    <div>
+      <form>
+                <div className="form-group">
+                  <p className="lead">
+                    <div className="form-group">
+                      <label for="exampleFormControlTextarea1">Biography:</label>
+                      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlSelect1">Has Pets:</label>
+                      <select className="form-control" id="exampleFormControlSelect1">
+                        <option>True</option>
+                        <option>False</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlSelect1">Smoker:</label>
+                      <select className="form-control" id="exampleFormControlSelect1">
+                        <option>True</option>
+                        <option>False</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlSelect1">Gender:</label>
+                      <select className="form-control" id="exampleFormControlSelect1">
+                        <option>Female</option>
+                        <option>Male</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label for="allergies">Allergies:</label>
+                      <input type="text" class="form-control" id="allergies" />
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlSelect1">Able to Meet Payments On Time:</label>
+                      <select className="form-control" id="exampleFormControlSelect1">
+                        <option>True</option>
+                        <option>False</option>
+                      </select>
+                    </div>
+                    <div className="form-group">
+                      <label for="exampleFormControlTextarea1">Weekend Activites:</label>
+                      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+
+
+                  </p>
+                </div>
+                <button type="submit" className="btn1">Submit</button>
+
+              </form>
+    </div>
+  )
+}
+
+export default ProfileCard;
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-
-
+import ProfileCard from './ProfileCard';
 
 class ProfilePage extends Component {
   render() {
@@ -13,65 +12,11 @@ class ProfilePage extends Component {
         <div className="card m-5">
           <div className="row m-5">
             <div className="col-md-5">
-
               <img src="http://via.placeholder.com/300x250" />
-
             </div>
-
             <div className="col-md-7">
-              <form>
-                <div className="form-group">
-                  <p className="lead">
-                    <div className="form-group">
-                      <label for="exampleFormControlTextarea1">Biography:</label>
-                      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <div className="form-group">
-                      <label for="exampleFormControlSelect1">Has Pets:</label>
-                      <select className="form-control" id="exampleFormControlSelect1">
-                        <option>True</option>
-                        <option>False</option>
-                      </select>
-                    </div>
-                    <div className="form-group">
-                      <label for="exampleFormControlSelect1">Smoker:</label>
-                      <select className="form-control" id="exampleFormControlSelect1">
-                        <option>True</option>
-                        <option>False</option>
-                      </select>
-                    </div>
-                    <div className="form-group">
-                      <label for="exampleFormControlSelect1">Gender:</label>
-                      <select className="form-control" id="exampleFormControlSelect1">
-                        <option>Female</option>
-                        <option>Male</option>
-                      </select>
-                    </div>
-                    <div className="form-group">
-                      <label for="allergies">Allergies:</label>
-                      <input type="text" class="form-control" id="allergies" />
-                    </div>
-                    <div className="form-group">
-                      <label for="exampleFormControlSelect1">Able to Meet Payments On Time:</label>
-                      <select className="form-control" id="exampleFormControlSelect1">
-                        <option>True</option>
-                        <option>False</option>
-                      </select>
-                    </div>
-                    <div className="form-group">
-                      <label for="exampleFormControlTextarea1">Weekend Activites:</label>
-                      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-
-
-                  </p>
-                </div>
-                <button type="submit" className="btn1">Submit</button>
-
-              </form>
-
+              <ProfileCard />
             </div>
-
           </div>
         </div>
       </div>
